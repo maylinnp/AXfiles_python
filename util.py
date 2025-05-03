2d9cccf7e5cee1f8009e151449d69f22440f8201
maylinnp <paulsen.maylinn@gmail.com>
2025-05-03 12:42:37 -0700

    start util file

---
+++ util.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+def get_matching_files(folder: str, pattern: str, extension: str) -> list:
+    """
+    Method that gets all files in a folder with a given unifying pattern and
+    specific extension. Will return a list sorted by filename
+
+    Args:
+        folder (str): folder in which to look for files
+        pattern (str): identifying file pattern
+        extension (str): file extension
+
+    Returns:
+        list: ordered by filename
+    """
+    folder_path = Path(folder)
+    return sorted(
+        [file for file in folder_path.glob(f"*{pattern}*{extension}") if file.is_file()]
+    )
